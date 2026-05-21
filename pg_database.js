85f9cac68176f995ebe2bdc677c8f49ab6ac84c9
Alex Wilson <arwwork1@gmail.com>
2026-05-21 22:46:24 +0100

    debug: log DB connection string host

--- pg_database.js
+++ pg_database.js
@@ -1,12 +1,19 @@
 const { Pool } = require("pg");
 
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString,
     ssl: { rejectUnauthorized: false },
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 10000
 });
+
+console.log('[DB] Connection string host:',
+    connectionString ?
+    connectionString.replace(/:([^@]+)@/, ':***@') :
+    'NOT SET'
+);
 
 pool.query('SELECT 1')
     .then(() => console.log('[DB] PostgreSQL connected successfully'))
